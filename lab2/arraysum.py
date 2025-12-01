495b7e497f7002975900a921b95dae43823cf3ec
Alex Karlsen <alex.j.karlsen@gmail.com>
2025-12-01 12:42:51 +0100

    Progress labb 2

--- lab2/arraysum.py
+++ lab2/arraysum.py
@@ -7,7 +7,6 @@ def genArray(n):
         n = n-1
     return array
 
-
 def algorithm1(array):
     if len(array)<=3:
         return False
@@ -21,19 +20,90 @@ def algorithm1(array):
         j += 1
     return algorithm1(array[1:])
 
-def algorithm2(array, dict):
-    if len(array)<=3:
+
+#def algorithm2(array, exTargets=None):
+    if exTargets is None:
+        exTargets = {}
+
+    if array==[]:
         return False
-    hm = dict
+
     target = array[0]
-    i = 0
-    while i<len(array):
-        if target-array[i] in hm:
+
+    for element in exTargets:
+        needed = target-element
+
+        if needed in exTargets and needed != element:
             return True
-        hm[array[i]] = ""
-        i += 1
-    return algorithm2(array[1:], hm)
+
+    exTargets[target] = ""
+    return algorithm2(array[1:], exTargets)
 
 
 
-print(algorithm2([1,1,1,1,3,5,2,3], {}))
+def algorithm2(A, n):
+
+    # Recursive base case, when only 2 elements in array, sort them and return false
+    # since 2 elements cant build x + y = z
+    if n < 3:
+
+        if A[0] > A[1]:
+            temp = A[0]
+            A[0] = A[1]
+            A[1] = temp
+        return False
+
+    if algorithm2(A, n - 1):
+        return True
+
+    # Set the last element of the usable length to the target
+    target = A[n - 1]
+
+    # Declare 2 pointers, one in the start and one in the end of the list
+    # Move pointer 2 down if too big and pointer 1 up if too small
+    left = 0
+    right = n - 2
+    while left < right:
+        current_sum = A[left] + A[right]
+        if current_sum == target:
+            return True
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+
+    # To check if target is a part of x + y = z but is not z, we use absolute value
+    # this checks if target is either x or y
+
+    target_diff = target
+    if target_diff < 0:
+        target_diff = -target_diff
+
+    # We declare 2 new pointers, one at first element and one at second. If this diff is too big, we
+    # try with the next pair instead, aka move up both pointers one step
+    left = 0
+    right = 1
+
+    while right < n - 1:
+        diff = A[right] - A[left]
+
+        if diff < target_diff:
+            right += 1
+        elif diff > target_diff:
+            left += 1
+            if left == right:
+                right += 1
+        else:
+            return True
+
+    # sort in the element which was being used as target to make sure list is sorted for next
+    # recursive run
+    j = n - 2
+    while j >= 0 and A[j] > target:
+        A[j + 1] = A[j]
+        j -= 1
+    A[j + 1] = target
+
+    return False
+
--- lab2/subarray.py
+++ lab2/subarray.py
@@ -1,28 +1,12 @@
-def maxSubarraySum(arr, res = 0, maxEnding = 0):
-    # Stores the result (maximum sum found so far)
-    res = res
+def maxSubarraySum2(arr, start, end):
 
-    # Maximum sum of subarray ending at current position
-    maxEnding = maxEnding
-
-    for i in range(1, len(arr)):
-        # Either extend the previous subarray or start
-        # new from current element
-        maxEnding = max(maxEnding + arr[i], arr[i])
-
-        # Update result if the new subarray sum is larger
-        res = max(res, maxEnding)
-
-    return res
-
-def maxSubarraySum2(arr):
-    if len(arr) == 1:
+    if  start == end:
         return arr[0], arr[0], arr[0], arr[0]
 
-    middle = len(arr) // 2
+    middle = (start + end)/2
 
-    left_max, left_prefix, left_suffix, left_total = maxSubarraySum2(arr[:middle])
-    right_max, right_prefix, right_suffix, right_total = maxSubarraySum2(arr[middle:])
+    left_max, left_prefix, left_suffix, left_total = maxSubarraySum2(arr, start, middle)
+    right_max, right_prefix, right_suffix, right_total = maxSubarraySum2(arr, middle, end)
 
     total = left_total + right_total
 
@@ -45,4 +29,3 @@ def maxSubarraySum2(arr):
     return max, prefix, suffix, total
 
 
-print(maxSubarraySum2([-5, -2, -9, -1, -8]))
